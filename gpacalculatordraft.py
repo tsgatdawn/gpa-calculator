9c718d9799f95b8921d1148511fb31365c76fa63
tsgatdawn <38501863+tsgatdawn@users.noreply.github.com>
2019-02-12 21:27:04 -0800

    added while loop

--- gpacalculatordraft.py
+++ gpacalculatordraft.py
@@ -4,78 +4,98 @@ grade_point_three = 0
 grade_point_four = 0
 
 
-grade_class_one = input ('What grade do you have in FAT? ')
-if grade_class_one == "A":
+while True:
+    grade_class_one = input('What grade do you have in class one? (Please enter a capital letter grade)? ')
+    if grade_class_one.lower() not in ('a', 'b', 'c', 'd', 'f'):
+        print('You inserted an invalid letter grade, try again.')
+        continue
+    else:
+        break
+if grade_class_one == "A" or grade_class_one == "a":
     grade_point_one += 4.0
-elif grade_class_one == "B":
+elif grade_class_one == "B" or grade_class_one == "b":
     grade_point_one += 3.0
-elif grade_class_one == "C":
+elif grade_class_one == "C" or grade_class_one == "c":
     grade_point_one += 2.0
-elif grade_class_one == "D":
+elif grade_class_one == "D" or grade_class_one == "d":
     grade_point_one += 1.0
-elif grade_class_one == "F":
+elif grade_class_one == "F" or grade_class_one == "f":
     grade_point_one += 0.0
-else :
-    print('You inserted a wrong grade!')
 
-print ('So in FAT, your grade is' ,grade_class_one)
+
+print ('So in class one, your grade is' ,grade_class_one)
 credit_class_one = input ('How much credit/s is the class worth? ')
-print ("In " + "FAT " + "you have " + credit_class_one + " credits.")
+print ("In " + "class one " + "you have " + credit_class_one + " credits.")
 
-grade_class_two = input ('What grade do you have in Writing 121? ')
+while True:
+    grade_class_two = input('What grade do you have in class two? (Please enter a capital letter grade)? ')
+    if grade_class_two.lower() not in ('a', 'b', 'c', 'd', 'f'):
+        print('You inserted an invalid letter grade, try again.')
+        continue
+    else:
+        break
 
-if grade_class_two == "A":
+
+if grade_class_two == "A" or grade_class_two == "a":
     grade_point_two += 4.0
-elif grade_class_two == "B":
+elif grade_class_two == "B" or grade_class_two == "b":
     grade_point_two += 3.0
-elif grade_class_two == "C":
+elif grade_class_two == "C" or grade_class_two == "c":
     grade_point_two += 2.0
-elif grade_class_two == "D":
+elif grade_class_two == "D" or grade_class_two == "d":
     grade_point_two += 1.0
-elif grade_class_two == "F":
+elif grade_class_two == "F" or grade_class_two == "f":
     grade_point_two += 0.0
-else:
-    print('You inserted a wrong grade!')
-print ('So in Writing 121, your grade is' ,grade_class_two)
+
+print ('So in class two, your grade is' ,grade_class_two)
 credit_class_two = input ('How much credit/s is the class worth? ')
-print ("In " + "Writing 121 " + "you have " + credit_class_one + " credits.")
+print ("In " + "class two " + "you have " + credit_class_one + " credits.")
 
-grade_class_three = input ('What grade do you have in CAD 1? ')
+while True:
+    grade_class_three = input('What grade do you have in class three? (Please enter a capital letter grade)? ')
+    if grade_class_three.lower() not in ('a', 'b', 'c', 'd', 'f'):
+        print('You inserted an invalid letter grade, try again.')
+        continue
+    else:
+        break
 
-if grade_class_three == "A":
+if grade_class_three == "A" or grade_class_three == "a":
     grade_point_three += 4.0
-elif grade_class_three == "B":
+elif grade_class_three == "B" or grade_class_three == "b":
     grade_point_three += 3.0
-elif grade_class_three == "C":
+elif grade_class_three == "C" or grade_class_three == "c":
     grade_point_three += 2.0
-elif grade_class_three == "D":
+elif grade_class_three == "D" or grade_class_three == "d":
     grade_point_three += 1.0
-elif grade_class_three == "F":
+elif grade_class_three == "F" or grade_class_three == "f":
     grade_point_three += 0.0
-else :
-    print('You inserted a wrong grade!')
-    print('You inserted a wrong grade!')
-print ('So in CAD 1, your grade is' ,grade_class_three)
+
+print ('So in class three, your grade is' ,grade_class_three)
 credit_class_three = input ('How much credit/s is the class worth? ')
-print ("In " + "CAD 1 " + "you have " + credit_class_one + " credits.")
+print ("In " + "class three " + "you have " + credit_class_one + " credits.")
 
-grade_class_four = input ('What grade do you have in AP Computer Science Principles? ')
+while True:
+    grade_class_four = input('What grade do you have in class four? (Please enter a capital letter grade)? ')
+    if grade_class_four.lower() not in ('a', 'b', 'c', 'd', 'f'):
+        print('You inserted an invalid letter grade, try again.')
+        continue
+    else:
+        break
 
-if grade_class_four == "A":
+if grade_class_four == "A" or grade_class_four == "a":
     grade_point_four += 4.0
-elif grade_class_four == "B":
+elif grade_class_four == "B" or grade_class_four == "b":
     grade_point_four += 3.0
-elif grade_class_four == "C":
+elif grade_class_four == "C" or grade_class_four == "c":
     grade_point_four += 2.0
-elif grade_class_four == "D":
+elif grade_class_four == "D" or grade_class_four == "d":
     grade_point_four += 1.0
-elif grade_class_four == "F":
+elif grade_class_four == "F" or grade_class_four == "f":
     grade_point_four += 0.0
-else :
-    print('You inserted a wrong grade!')
-print('So in AP Computer Science Principles, your grade is' ,grade_class_four)
+
+print('So in class four, your grade is' ,grade_class_four)
 credit_class_four = input ('How much credit/s is the class worth? ')
-print("In " + "AP Computer Science Principles " + "you have " + credit_class_one + " credits.")
+print("In " + "class four " + "you have " + credit_class_one + " credits.")
 
 total_gpa = (grade_point_one + grade_point_two + grade_point_three + grade_point_four) / 4
 print("Your GPA is: " + str(total_gpa))
