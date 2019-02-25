9b6c0b8ffea6fb73fe160d88a4fb573a84e70c15
tsgatdawn <38501863+tsgatdawn@users.noreply.github.com>
2019-02-24 23:14:30 -0800

    added back rough draft

--- gpacalculatordraft.py
+++ gpacalculatordraft.py
@@ -1,2 +1,102 @@
+grade_point_one = 0
+grade_point_two = 0
+grade_point_three = 0
+grade_point_four = 0
 
+
+while True:
+    grade_class_one = input('What grade do you have in class one? (Please enter a capital letter grade)? ')
+    if grade_class_one.lower() not in ('a', 'b', 'c', 'd', 'f'):
+        print('You inserted an invalid letter grade, try again.')
+        continue
+    else:
+        break
+if grade_class_one == "A" or grade_class_one == "a":
+    grade_point_one += 4.0
+elif grade_class_one == "B" or grade_class_one == "b":
+    grade_point_one += 3.0
+elif grade_class_one == "C" or grade_class_one == "c":
+    grade_point_one += 2.0
+elif grade_class_one == "D" or grade_class_one == "d":
+    grade_point_one += 1.0
+elif grade_class_one == "F" or grade_class_one == "f":
+    grade_point_one += 0.0
+
+
+print ('So in class one, your grade is' ,grade_class_one)
+credit_class_one = input ('How much credit/s is the class worth? ')
+print ("In " + "class one " + "you have " + credit_class_one + " credits.")
+
+while True:
+    grade_class_two = input('What grade do you have in class two? (Please enter a capital letter grade)? ')
+    if grade_class_two.lower() not in ('a', 'b', 'c', 'd', 'f'):
+        print('You inserted an invalid letter grade, try again.')
+        continue
+    else:
+        break
+
+
+if grade_class_two == "A" or grade_class_two == "a":
+    grade_point_two += 4.0
+elif grade_class_two == "B" or grade_class_two == "b":
+    grade_point_two += 3.0
+elif grade_class_two == "C" or grade_class_two == "c":
+    grade_point_two += 2.0
+elif grade_class_two == "D" or grade_class_two == "d":
+    grade_point_two += 1.0
+elif grade_class_two == "F" or grade_class_two == "f":
+    grade_point_two += 0.0
+
+print ('So in class two, your grade is' ,grade_class_two)
+credit_class_two = input ('How much credit/s is the class worth? ')
+print ("In " + "class two " + "you have " + credit_class_one + " credits.")
+
+while True:
+    grade_class_three = input('What grade do you have in class three? (Please enter a capital letter grade)? ')
+    if grade_class_three.lower() not in ('a', 'b', 'c', 'd', 'f'):
+        print('You inserted an invalid letter grade, try again.')
+        continue
+    else:
+        break
+
+if grade_class_three == "A" or grade_class_three == "a":
+    grade_point_three += 4.0
+elif grade_class_three == "B" or grade_class_three == "b":
+    grade_point_three += 3.0
+elif grade_class_three == "C" or grade_class_three == "c":
+    grade_point_three += 2.0
+elif grade_class_three == "D" or grade_class_three == "d":
+    grade_point_three += 1.0
+elif grade_class_three == "F" or grade_class_three == "f":
+    grade_point_three += 0.0
+
+print ('So in class three, your grade is' ,grade_class_three)
+credit_class_three = input ('How much credit/s is the class worth? ')
+print ("In " + "class three " + "you have " + credit_class_one + " credits.")
+
+while True:
+    grade_class_four = input('What grade do you have in class four? (Please enter a capital letter grade)? ')
+    if grade_class_four.lower() not in ('a', 'b', 'c', 'd', 'f'):
+        print('You inserted an invalid letter grade, try again.')
+        continue
+    else:
+        break
+
+if grade_class_four == "A" or grade_class_four == "a":
+    grade_point_four += 4.0
+elif grade_class_four == "B" or grade_class_four == "b":
+    grade_point_four += 3.0
+elif grade_class_four == "C" or grade_class_four == "c":
+    grade_point_four += 2.0
+elif grade_class_four == "D" or grade_class_four == "d":
+    grade_point_four += 1.0
+elif grade_class_four == "F" or grade_class_four == "f":
+    grade_point_four += 0.0
+
+print('So in class four, your grade is' ,grade_class_four)
+credit_class_four = input ('How much credit/s is the class worth? ')
+print("In " + "class four " + "you have " + credit_class_one + " credits.")
+
+total_gpa = (grade_point_one + grade_point_two + grade_point_three + grade_point_four) / 4
+print("Your GPA is: " + str(total_gpa))
 
--- gpacalculatorproject.py
+++ gpacalculatorproject.py
@@ -1,5 +1,6 @@
 gradeDict = {"A": 4.0, "B": 3.0, "C": 2.0, "D": 1.0, "F": 0}
 ans = 0
+
 # Asking user how many classes they would like to calculate GPA for
 while not ans:
     try:
@@ -10,8 +11,8 @@ while not ans:
         ans = 0
         print("Please insert a realistic number.")
 # Using a while loop to stop whenever the user reached their class count
-gradeSum = 0
 for i in range(0, ans):
+    gradeSum = 0
     # Asking user what grade they have for the class
     while True:
         grade_of_class = input("What is the letter grade of your first class?")
@@ -20,12 +21,14 @@ for i in range(0, ans):
             continue
         else:
             grade_of_class = grade_of_class.upper()
-            break
+    else:
+        break
+
+
 # Using dictionary to convert from letter to numeric point-grade
     numeric_grade_of_class = gradeDict[grade_of_class]
-    gradeSum = gradeSum + numeric_grade_of_class
-    gpa = gradeSum / ans
-    print(gpa)
+    numeric_grade_of_class = numeric_grade_of_class + gradeSum;
+    gpa = numeric_grade_of_class / ans
 
 
 
