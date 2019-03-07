4da293f80fa1ac0d3d944a8dcba11ddb628cd1ba
tsgatdawn <38501863+tsgatdawn@users.noreply.github.com>
2019-03-06 23:27:58 -0800

    added credits

--- gpacalculatorfinal.py
+++ gpacalculatorfinal.py
@@ -1,5 +1,8 @@
 gradeDict = {"A": 4.0, "B": 3.0, "C": 2.0, "D": 1.0, "F": 0}
 ans = 0
+cred = 0
+credSum = 0
+gradePointSum = 0
 # Asking user how many classes they would like to calculate GPA for
 while not ans:
     try:
@@ -18,11 +21,34 @@ for i in range(0, ans):
         if grade_of_class.lower() not in ('a', 'b', 'c', 'd', 'f'):
             print('You inserted an invalid letter grade, try again.')
             continue
+
         else:
             grade_of_class = grade_of_class.upper()
-            break
-# Calculate GPA
+
+            try:
+                cred = float(input("Enter the credits this class is worth."))
+            except ValueError:
+                print("Please enter only numeric values")
+                continue
+            else:
+                gradePoint = gradeDict[grade_of_class] * cred
+            gradePointSum = gradePointSum + gradePoint
+            credSum = credSum + cred
+        break
+
+
+
+
+
+# Calculate GPA and credits
     numeric_grade_of_class = gradeDict[grade_of_class]
     gradeSum = gradeSum + numeric_grade_of_class
-    GPA = gradeSum / ans
-print(GPA)
+    gpa = gradePointSum / credSum
+# Code gotten from https://stackoverflow.com/questions/9232256/round-up-to-second-decimal-place-in-python (To round GPA)
+from math import ceil
+
+num = gpa
+num = ceil(num * 100) / 100.0
+roundedGPA = str(num)
+print("Your GPA is: " + roundedGPA)
+
